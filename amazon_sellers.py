3d83da1446ddc743c49f1fb5038cfdfd88ec12e4
mDanone <daniyalbekow@icloud.com>
2021-01-27 15:45:38 +0300

    finally made scrapper

--- amazon_sellers.py
+++ amazon_sellers.py
@@ -1,6 +1,8 @@
 import os
 import time
 import csv
+import datetime
+import re
 
 import xlrd
 from selenium import webdriver
@@ -9,18 +11,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 
 PATH = f"{os.getcwd()}\\drivers\\chromedriver.exe"
 options = webdriver.chrome.options.Options()
 driver = webdriver.Chrome(executable_path=PATH)
 
+# Список дерева заданных путей к категориям
 book = xlrd.open_workbook("grocery_browse.xls")
 list_of_tree = []
 excel_worksheet = book.sheet_by_index(1)
-
 for row in range(1, excel_worksheet.nrows):
     list_of_tree.append(int(excel_worksheet.cell_value(row, 0)))
+print(list_of_tree)
 
 
 def scrapeElementsFromUl(div_with_uls):
@@ -46,7 +50,9 @@ def scrapeElementsFromUl(div_with_uls):
 
 def get_seller_id(seller_link):
     '''Находим id продавцы в html'''
-    ActionChains(driver).click(seller_link).perform()
+    ActionChains(driver).key_down(Keys.CONTROL).click(seller_link).key_up(Keys.CONTROL).perform()
+    driver.switch_to.window(driver.window_handles[1])
+    print("Открыл продавца")
     try:
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((
@@ -66,7 +72,7 @@ def getInfoAboutProducts():
     pages = int(driver.find_element_by_xpath(
         '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[2]/div[17]/span/div/div/ul/li[6]'
         ).text)
-    for _ in range(pages - 149):
+    for _ in range(pages - 45):
         # time.sleep нужен чтобы успели прогрузиться все продуктов на странице
         # Так как страница динамическая, сложно отслеживать нахождение всех элементов при помощи WebDriverWait
         # 1.5 секунд достаточно для загрузки
@@ -75,14 +81,33 @@ def getInfoAboutProducts():
 
         for product_name in product_block:
             name = product_name.find_element_by_css_selector(".a-size-medium.a-color-base.a-text-normal")
-            price = product_name.find_element_by_css_selector(".a-price")
-            price = ".".join(price.text.split("$")[1].split("\n"))
+            try:
+                # Количество элементов в наборе
+                num_of_elems = re.findall("\d+", name.text.split("(")[1])[0]
+                print(num_of_elems)
+            except:
+                num_of_elems = 1
+                print(num_of_elems)
+            try:
+                price = product_name.find_element_by_css_selector(".a-price")
+                price = ".".join(price.text.split("$")[1].split("\n"))
+            except NoSuchElementException:
+                price = 'No price'
             count_reviews = product_name.find_element_by_css_selector(".a-size-base")
-            count_reviews = "".join(count_reviews.text.split(','))
-            rating = product_name.find_element_by_xpath(".//div[@class='a-section a-spacing-none a-spacing-top-micro']/div/span[1]")
-            rating = rating.get_attribute("aria-label").split(' ')[0]
+
+            # Делаем проверку на начало ревью с доллара, так как если нет ревью то соханяется цена за штуку товара
+            if count_reviews.text.startswith("$"):
+                count_reviews = "No reviews"
+            else:
+                count_reviews = "".join(count_reviews.text.split(','))
+
+            try:
+                rating = product_name.find_element_by_xpath(".//div[@class='a-section a-spacing-none a-spacing-top-micro']/div/span[1]")
+                rating = rating.get_attribute("aria-label").split(' ')[0]
+            except NoSuchElementException:
+                rating = "No rating"
             # rating.split(" ")[0] нужен для того чтобы разделить строк [<rate>] out of 5 нам нужен только rate
-            product_text.append([name.text, price, count_reviews, rating])
+            product_text.append([name.text, price, count_reviews, rating, num_of_elems])
             print("Добавляю текст")
         try:
             WebDriverWait(driver, 10).until(
@@ -99,6 +124,7 @@ def getInfoAboutProducts():
 
 def writeZipCode():
     '''Ставим зип код Сша чтобы отображались все продукты Поставищка'''
+    driver.get("https://www.amazon.com/ref=nav_logo")
     driver.find_element_by_xpath('//*[@id="glow-ingress-line2"]').click()
     try:
         zip_code = WebDriverWait(driver, 20).until(
@@ -115,12 +141,16 @@ def writeZipCode():
 def openTree(tree_id):
     '''Открываем Топ продавцов в нужной нам категории товаров'''
     driver.get(f"https://www.amazon.com/gp/search/other/?pickerToList=enc-merchantbin&node={tree_id}")
-    writeZipCode()
-    div_with_uls = driver.find_element_by_id("ref_275225011")
+    try:
+        div_with_uls = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "refinementList"))
+            )
+    except TimeoutException:
+        print("Не удалось найти продавца")
     return div_with_uls
 
 
-def getSellerInfo(seller_id):
+def getSellerInfo(tree_id, seller_id):
     '''
     Здесь переходим к товарам продавца
     Здесь же нужно будет собрать всю инфу о продавце и занести в csv файл
@@ -131,26 +161,40 @@ def getSellerInfo(seller_id):
     count_reviews = driver.find_element_by_xpath('//*[@id="feedback-summary-table"]/tbody/tr[5]/td[5]/span').text
     rating = driver.find_element_by_xpath('//*[@id="seller-feedback-summary"]/i[1]/span').get_attribute("innerHTML").split(' ')[0]
     address = ' '.join(driver.find_element_by_xpath('//*[@id="seller-profile-container"]/div[2]/div/ul/li[2]/span/ul').text.split('\n'))
-    zipcode = driver.find_element_by_xpath('//*[@id="seller-profile-container"]/div[2]/div/ul/li[2]/span/ul').text.split('\n')[-2]
-    seller_info = [name, merchantID, rating, count_reviews, address, zipcode]
 
     driver.find_element_by_xpath('//*[@id="products-link"]/a').click()
+    seller_info = [name, merchantID, rating, count_reviews, address, 
+                   tree_id, driver.current_url,
+                   f"https://www.amazon.com/sp?_encoding=UTF8&seller={merchantID}"]
+    with open(f"saved_csv_merchant\\{tree_id}_merchant", 'a', encoding='utf-8') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(seller_info)
+
     print(seller_info)
     return seller_info
 
 
-if __name__ == "__main__":
-    '''
-    тут нужно будет скомпоновать все в одну функции чтобы
-    запускать все одной кнопкой используя нужные аргументы
-    '''
-    div_with_uls = openTree(16323201)
-    list_of_links = scrapeElementsFromUl(div_with_uls)
-    seller_id = get_seller_id(list_of_links[0])
-    getSellerInfo(seller_id)
-    result = getInfoAboutProducts()
-    with open('result.csv', 'w', encoding='utf-8') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(["product`s name", "price", "count_reviews", "rating"])
-        for element in result:
-            csvwriter.writerow((element[0], element[1], element[2], element[3]))
+def run_amazon_scraper(list_of_tree):
+    writeZipCode()
+    for tree in list_of_tree:
+        div_with_uls = openTree(int(tree))
+        list_of_links = scrapeElementsFromUl(div_with_uls)
+        for link in list_of_links:
+            seller_id = get_seller_id(link)
+            seller_info = getSellerInfo(int(tree), seller_id)
+            result = getInfoAboutProducts()
+
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+
+            with open(f'saved_csvs\\{tree}.csv', 'a', encoding='utf-8') as csvfile:
+                csvwriter = csv.writer(csvfile)
+                csvwriter.writerow(["product`s name", "price", "count_reviews", "rating"])
+                for product_info in result:
+                    csvwriter.writerow((datetime.date.today(), seller_info[1], seller_info[0], seller_info[-1],
+                                        seller_info[-2], product_info[4], seller_info[4],
+                                        product_info[0], product_info[1],
+                                        product_info[2], product_info[3]))
+
+
+run_amazon_scraper(list_of_tree)
